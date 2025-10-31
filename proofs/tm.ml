7bdbee0bd3837e065c97224990944485cb08b65e
Stefan O'Rear <sorear@fastmail.com>
2025-10-31 03:50:55 -0400

    proof: named states, prove return*

--- proofs/tm.ml
+++ proofs/tm.ml
@@ -1,6 +1,42 @@
 let _ = prioritize_int();;
+let _ = set_verbose_symbols(false);;
+
+(* hol-light usage notes
+
+   still underusing directed conversions, implicational and target rewriting,
+   custom tactics, simpsets, user parsers/printers *)
+
+(* subst a large term into the body of an abs is slow since it needs to be checked for bound variables; possibly exponential nested alpha, def linear *)
+(* comparing pointer identical terms is fast; at worst linear if no alpha convert *)
+(* assumption lists are linear *)
+(* substitution always linear in template, except when alpha converting *)
+(* once_depth: try root, try children once if failed *)
+(* depth: exhaust children then root *)
+(* redepth: recurse, try root once, repeat *)
+(* top_depth: exhaust, recurse, alternate trying root once and recursing *)
+(* top_sweep: exhaust then recurse *)
+(* definition sizes irrelevant, definitions list not used by core; too many (hundreds) of constants will slow down parsing *)
+
+(*
+let my_rev_conv =
+   let lem1 = SYM (REWRITE_CONV [APPEND_NIL] `APPEND (REVERSE (xs:A list)) []`) in
+   let lem2 = REWRITE_CONV [REVERSE;APPEND;GSYM APPEND_ASSOC] `APPEND (REVERSE (CONS (x:A) xs)) ys` in
+   let lem3 = REWRITE_CONV [REVERSE;APPEND] `APPEND (REVERSE []) (ys:A list)` in
+   fun rterm ->
+      let lsterm = rand rterm in
+      let Tyapp (_, ty::_) = type_of lsterm in
+      let [lem1i; lem2i; lem3i] = map (INST_TYPE [ty,aty]) [lem1; lem2; lem3] in
+      let [vxs; vx; vys] = frees (concl lem2i) in
+      let rec iter eq lhs rhs = match lhs with
+         | Const ("NIL",_) -> TRANS eq (INST [rhs,vys] lem3i)
+         | Comb (Comb (_,l) as cons_l,rest) -> iter (TRANS eq (INST [l,vx;rest,vxs;rhs,vys] lem2i)) rest (mk_comb(cons_l,rhs))
+      in iter (INST [lsterm,vxs] lem1i) lsterm (mk_const("NIL",[ty,aty])) ;;
+my_rev_conv `REVERSE [1;2;3;4;5;6]`;;
+*)
 
 (* preliminaries - function and list handling *)
+
+let NUM_OF_INT_2 = prove(`&0 <= x ==> ?z. x:int = &z`, REWRITE_TAC[EXISTS_THM;NUM_OF_INT;num_of_int] THEN MESON_TAC[]);;
 
 let ITERF_DEF = define`ITERF 0 f (x:A) = x /\ ITERF (SUC n) f x = f (ITERF n f x)`;;
 let ITERF_ADD = prove(`!m n f (x:A). ITERF (m + n) f x = ITERF m f (ITERF n f x)`,
@@ -40,7 +76,9 @@ let DROP_APPEND_GE = prove(
    similar to sptree from HOL4, but simplified for our use case
    
    imprecise = few guarantees on value outside provided alist. a precise mode
-   is possible but requires ~twice the term nodes *)
+   is possible but requires ~twice the term nodes
+   
+   todo: actual definition primitive, maybe a conv-generator *)
 
 let nmap_node_DEF = define`NMAP_NODE l r = \i. (if ODD i then r else l) (i DIV 2):A` ;;
 let nmap_leaf_DEF = define`NMAP_LEAF v f = \i. if i = 0 then v else f:A` ;;
@@ -115,16 +153,6 @@ let state_info = List.map toks_to_state_info tm_states_tok ;;
 
 let mk_bool b = if b then `T` else `F` ;;
 
-(* subst a large term into the body of an abs is slow since it needs to be checked for bound variables; possibly exponential nested alpha, def linear *)
-(* comparing pointer identical terms is fast; at worst linear if no alpha convert *)
-(* assumption lists are linear *)
-(* substitution always linear in template, except when alpha converting *)
-(* once_depth: try root, try children once if failed *)
-(* depth: exhaust children then root *)
-(* redepth: recurse, try root once, repeat *)
-(* top_depth: exhaust, recurse, alternate trying root once and recursing *)
-(* top_sweep: exhaust then recurse *)
-
 (* construct and prove validity of loaded transition table *)
 
 let transition_table_DEF =
@@ -137,8 +165,8 @@ let transition_table_LIMIT = CONV_RULE (SIMP_CONV[]) ((PURE_REWRITE_CONV [transi
 
 let transition_table_VALID = prove(`gtm_valid transition_table 748`, REWRITE_TAC[gtm_valid_DEF;transition_table_LIMIT;FORALL_BOOL_THM;transition_table_DEF;nmap_CLAUSES;bmap_CLAUSES;ARITH_EQ]);;
 
-    (* to convert ground terms, may become optimized *)
-let transition_table_CONV = REWRITE_CONV[transition_table_DEF;nmap_CLAUSES;bmap_CLAUSES];;
+    (* 200 times faster than REWRITE_CONV on a ground term *)
+let transition_table_CONV = GEN_REWRITE_CONV DEPTH_CONV [transition_table_DEF;nmap_CLAUSES;bmap_CLAUSES];;
 
 (* tape handling
 
@@ -148,16 +176,19 @@ let transition_table_CONV = REWRITE_CONV[transition_table_DEF;nmap_CLAUSES;bmap_
    because ajwade's machines use an unclean initialization process.
 
    the initialization process itself does not have to be modeled since it
-   halts, but we need a calculation-friendly tape representation *)
+   halts, but we need a calculation-friendly tape representation
+   
+   this whole thing is incredibly circuitous for something that could have been
+   a definition. the two-sided zipper is almost certainly the interface we want
+   downstream, although the lists could be replaced with colists (wrapper
+   around num->bool) if the appropriate utilities existed (mostly, a version of
+   APPEND :A list -> (num -> A) -> num -> A). we could adopt the two-sided
+   zipper as a definition, or a symmetric zipper, or perhaps introduce colists
+   as an intermediate stage *)
 
 let list_tape_DEF = define `list_tape l tp = \i. &0 <= tp+i /\ tp+i < &(LENGTH l) /\ EL (num_of_int (tp+i)) l`;;
-(*
-let bilist_tape_DEF = define `bilist_tape ll sy rl = \i. if i = &0 then sy else let l = if i < &0 then ll else rl in let ii = num_of_int (abs i - 1) in ii < LENGTH l /\ EL ii l`;;
-*)
 
 let list_tape_SHIFT = prove(`shift j (list_tape l tp) = list_tape l (tp+j)`, SIMP_TAC[tape_shift_DEF;list_tape_DEF;INT_ADD_ASSOC]);;
-
-let NUM_OF_INT_2 = prove(`&0 <= x ==> ?z. x:int = &z`, REWRITE_TAC[EXISTS_THM;NUM_OF_INT;num_of_int] THEN MESON_TAC[]);;
 
 let list_tape_RAPPEND = prove(
  `list_tape (APPEND l [F]) tp = list_tape l tp`,
@@ -232,10 +263,6 @@ let zip_read = prove(
   REWRITE_TAC[INT_OF_NUM_CLAUSES; NUM_OF_INT_OF_NUM; LE_0; ADD;
     LENGTH_REVERSE; EL_APPEND; LT_REFL; SUB_REFL; EL; HD; ADD_AC;
     ARITH_RULE `x < x + y + 1`]);;
-(*
-let bilist_tape_READ = prove(`bilist_tape ll sy rl (&0) = sy`, REWRITE_TAC[bilist_tape_DEF]);;
-let bilist_tape_WRITE = prove(`write sy' (bilist_tape_2 ll sy rl) = (bilist_tape_2 ll sy' rl)`, SIMP_TAC[bilist_tape_DEF;tape_write_DEF]);;
-*)
 
 (* semantics
 
@@ -253,7 +280,13 @@ let bilist_tape_WRITE = prove(`write sy' (bilist_tape_2 ll sy rl) = (bilist_tape
 
    extend to a small-step semantics on "classes" (sets of worldstates); this
    roughly matches Hoare triples but always terminates and the operations
-   performed are implicit in the states *)
+   performed are implicit in the states
+   
+   reflexive small-step may be an option if we bounce between two disjoint sets
+   to prove non-termination, could also be expressed by having progressing and
+   non-progressing relations, a fourth option would be to extend the
+   completeness proof to prove that a register increases without bound while a
+   turing machine that halts will do so after finitely many writes *)
 
 let _ = parse_as_infix("-->_w",(12,"right"));;
 let tm_evolves = define`w1 -->_w w2 <=>
@@ -262,3 +295,132 @@ let tm_evolves = define`w1 -->_w w2 <=>
 let tm_evolves_TRANS = prove(`w1 -->_w w2 /\ w2 -->_w w3 ==> w1 -->_w w3`,
   REWRITE_TAC[tm_evolves] THEN INTRO_TAC "(@n1. im1) (@n2. im2)" THEN
   EXISTS_TAC `n2 + SUC n1` THEN ASM_REWRITE_TAC[GSYM ADD; ITERF_ADD]);;
+
+let tm_evolves_BASE = prove(
+ `gtm_step transition_table w1 = w2 ==> w1 -->_w w2`,
+ DISCH_TAC THEN REWRITE_TAC[tm_evolves] THEN EXISTS_TAC `0` THEN
+ ASM_REWRITE_TAC[ITERF_DEF]);;
+
+(* naming TM states
+
+   we don't want to name every state, partly for performance but mostly because
+   the names are only useful in hand-written proofs. the various dispatch tree
+   states will remain nameless, known only by their behavior
+   
+   it remains to be seen how we handle variance between tm versions. there are
+   only two basic versions of the register machine but some of them have
+   optimizations, dec_check, dec_restore, dec_scan_done are half states, !ENTRY
+   is unneeded, etc, as well as yucky issues with name collapsing
+   
+   todo: generalize bmap-based definition mechanism
+   todo: custom parse for tm states *)
+
+let defn_for_state tm name = new_basic_definition(mk_eq(tm,
+  mk_small_numeral(state_of_name name)));;
+let defn_state_pair tm name0 name1 = new_basic_definition(mk_eq(tm,
+  mk_bmap (mk_small_numeral (state_of_name name0))
+          (mk_small_numeral (state_of_name name1))));;
+let defn_state_q tm n0 n1 n2 n3 = new_basic_definition(mk_eq(tm,
+  mk_bmap (mk_bmap (mk_small_numeral (state_of_name n0))
+                   (mk_small_numeral (state_of_name n1)))
+          (mk_bmap (mk_small_numeral (state_of_name n2))
+                   (mk_small_numeral (state_of_name n3)))));;
+let dec_init_S = defn_for_state `dec_init:num` "dec.init";;
+let dec_check_S = defn_for_state `dec_check:num` "dec.check";;
+let dec_restore_S = defn_for_state `dec_restore:num` "dec.restore";;
+let dec_scan_S = defn_state_pair `dec_scan:bool->num` "dec.scan_0" "dec.scan_1";;
+let dec_scan_done_S = defn_for_state `dec_scan_done:num` "dec.scan_done";;
+let dec_shift_S = defn_state_pair `dec_shift:bool->num` "dec.shift_0" "dec.shift_1";;
+let inc_shift_S = defn_state_pair `inc_shift:bool->num` "inc.shift_0" "inc.shift_1";;
+let return_S = defn_state_q `return:bool->bool->num`
+    "return.0" "return.1" "return2.0" "return2.1";; 
+let dispatchroot_S = defn_for_state `dispatchroot:num` "main()[]";;
+let dispatch_S = defn_state_pair `dispatch:bool->num` "dispatch.0.carry" "nextstate_2";;
+
+(* register operations
+
+   start by proving single steps, use induction to build the inner loops, then
+   inductively construct the behavior of primitive register operations at the
+   dispatch/register interface boundary *)
+
+let REG = define`REG n xs = APPEND (REPLICATE (SUC n) T) (CONS F xs)`;;
+let REGFILE = define`REGFILE ns xs = ITLIST REG ns xs`;;
+let OPSEG = define`OPSEG ns cruft = CONS F (CONS F (ITLIST REG ns cruft))`;;
+let DEC_CRUFT = define`DEC_CRUFT n (creg,cbit) =
+        if n = 0 then creg,cbit else creg,CONS F cbit`;;
+let (REGLIKE, REGLIKE_IND, REGLIKE_CASES) = new_inductive_definition
+ `REGLIKE F [F] /\
+  (!bs. REGLIKE F bs ==> REGLIKE T (CONS F bs)) /\
+  (!b bs. REGLIKE T bs ==> REGLIKE b (CONS T bs))`;;
+
+(* TODO automate *)
+let return_LEMMA = prove(
+`return skip F,lzip_tape (CONS F ls) rs -->_w dispatch skip,lzip_tape ls (CONS F rs) /\
+ return skip F,lzip_tape (CONS T ls) rs -->_w return skip T,lzip_tape ls (CONS T rs) /\
+ return skip T,lzip_tape (CONS F ls) rs -->_w return skip F,lzip_tape ls (CONS F rs) /\
+ return skip T,lzip_tape (CONS T ls) rs -->_w return skip T,lzip_tape ls (CONS T rs)`,
+  BOOL_CASES_TAC `skip:bool` THEN REPEAT CONJ_TAC THEN MATCH_MP_TAC tm_evolves_BASE THEN
+  REWRITE_TAC[dispatch_S; return_S; gtm_step_DEF; zip_write; zip_read; bmap_CLAUSES] THEN
+  CONV_TAC transition_table_CONV THEN SIMP_TAC[LET_DEF;ARITH;LET_END_DEF; zip_shift]);;
+
+let return_THM = prove(
+`!b rf. REGLIKE b rf ==> !left right. (return skip b, lzip_tape (APPEND rf left) right) -->_w
+ (dispatch skip, lzip_tape left (APPEND (REVERSE rf) right))`,
+ MATCH_MP_TAC REGLIKE_IND THEN
+ REPEAT STRIP_TAC THEN REWRITE_TAC[APPEND; REVERSE; GSYM APPEND_ASSOC] THENL [
+   ALL_TAC;
+   TRANS_TAC (GEN_ALL tm_evolves_TRANS) `return skip F,lzip_tape (APPEND bs left) (CONS F right)`;
+   TRANS_TAC (GEN_ALL tm_evolves_TRANS) `return skip T,lzip_tape (APPEND bs left) (CONS T right)`] THEN
+ TRY (BOOL_CASES_TAC `b:bool`) THEN ASM_REWRITE_TAC[return_LEMMA]);;
+
+(*
+`!P. (!l b. (P F l ==> P T (CONS F l)) /\ (P T l ==> P b (CONS T l)))
+ ==> P T (CONS F (REGFILE ns rest)) /\ !s. P s (REGFILE ns rest)`
+`!ns. REGLIKE incz rf ==> (return skip incz, lzip_tape (APPEND (REVERSE rf left) right) -->_w (nextstate skip, lzip_tape left (APPEND rf right))`
+
+(* dec Z return.1 left-before 0 before reg *)
+(* dec NZ return.0 left-after 0 before reg *)
+(* inc return.0 left-after 0 after last reg *)
+(* init return.1 left-before 0 after last reg *)
+
+
+`(return skip T, lzip_tape (REGFILE (REVERSE nsl) (CONS F left)) (REGFILE nsr cruft)) -->_w
+ (nextstate skip, lzip_tape left (OPSEG (APPEND nsl nsr) cruft))`
+
+`!ns. (reg_decr 2, rzip_tape left (OPSEG (CONS n0 (CONS n1 ns)) cruft)) -->_w
+      (nextstate (~(n1 = 0)), lzip_tape left
+        (OPSEG (CONS n0 (CONS (PRE n1) ns)) (DEC_CRUFT n1 cruft)))`
+`?cruft'. (reg_incr 2, rzip_tape left (OPSEG (CONS n0 (CONS n1 ns)) cruft)) -->_w
+      (nextstate F, lzip_tape left
+        (OPSEG (CONS n0 (CONS (SUC n1) ns)) cruft'))`
+
+`{ dispatchroot, rzip_tape lcruft (APPEND (REVERSE pc) (OPSEG ns rcruft)) | T }`
+*)
+
+(* dispatch basics
+
+   dispatch and operations are two systems with clearly separated halves of the
+   tape, subsets of the state set, and only a handful of crossing states (2
+   dispatch, 2*nreg+1 operations)
+
+   (we may also consider an "initialization segment" which is just executed and
+   sets up the registers, although the 2017 machine runs reg_init
+   continuously...)
+
+   jumps and halting do not use data and happen entirely within the dispatch
+   segment. since jumps can go in and out of several subroutines, ignoring them
+   at higher levels is problematic, but we cannot identify them with either an
+   entry or exit state. instead, higher levels use dispatch-root states, which
+   are reliably hit before every operation or jump
+   
+   here we prove that from the dispatch-root we can reach an operation state if
+   there is one, or go to another dispatch-root if there was a jump, and that
+   operation returns can reach the dispatch root. it is mostly just expanding
+   definitions, the tricky part is parameterizing subroutines so that they can
+   be handled in any context *)
+
+(* .subs semantics
+   
+   combines the judgements from "dispatch basics" and "operations" and wraps it
+   all in existential quantifiers so that you don't need to study cruft
+   evolution *)
